e61b0d6a4e77959d3fbd8c1039f1034ebe0d2497
Mikael Bergström <krank23@gmail.com>
2021-08-30 10:57:13 +0200

    Initial commit

---
+++ TE19A_Klasser/Enemy.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TE19A_Klasser
+{
+  public class Enemy
+  {
+    public int x;
+    public int y;
+
+    public int hp;
+
+    public int damage;
+  }
+}
---
+++ TE19A_Klasser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TE19A_Klasser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      Enemy goomba = new Enemy();
+      Enemy fireFlower = new Enemy();
+
+      goomba.x = 200;
+
+      Console.WriteLine(fireFlower.x);
+
+      Console.ReadLine();
+    }
+  }
+}
